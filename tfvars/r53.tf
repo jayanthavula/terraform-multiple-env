8295bb6b06c327a2bbf85a3bf3b6ff96bb9590f9
jayanthavula <avulajayanth1997@gmail.com>
2025-02-07 20:09:13 +0530

    terrafrom-multi-env

--- tfvars/r53.tf
+++ tfvars/r53.tf
@@ -1,12 +1,12 @@
-# resource "aws_route53_record" "expense" {
-#   #count = 3
-#   count = length(var.instances)
-#   zone_id = var.zone_id
-#   name = "${var.instances[count.index]}.${var.domain_name}"  #interpolation
-#   type = "A"
-#   ttl = 1
-#   records = [aws_instance.expense[count.index].private_ip]
-# }
+resource "aws_route53_record" "expense" {
+  #count = 3
+  count = length(var.instances)
+  zone_id = var.zone_id
+  name = var.instances[count.index] == "frontend" && var.environment == "prod" ? var.domain_name : "${var.instances[count.index]}-${var.environment}.${var.domain_name}" #interpolation
+  type = "A"
+  ttl = 1
+  records = [var.instances[count.index] == "frontend" && var.environment == "prod" ? aws_instance.expense[count.index].public_ip : aws_instance.expense[count.index].private_ip]
+}
 
 # resource "aws_route53_record" "frontend" {
 #   allow_overwrite = true  
---
+++ workspaces/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.84.0"
+    }
+  }
+
+    backend "s3" {
+      bucket = "jaydevops-bucket"
+      key = "workspace-demo" # you should have uniqe keys with the bucket, same key should not be used in other repos or tf project
+      region = "us-east-1"
+      dynamodb_table = "jaydevops-table"
+    }
+}
+
+provider "aws" {
+  #Configuration options
+  region = "us-east-1"
+}
